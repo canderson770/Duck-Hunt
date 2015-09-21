5b9fe101c5c3e66d1b0918070db0afa6b8f7a5bc
unknown <10600006@CS514-13.ad.uvu.edu>
2015-09-21 10:07:07 -0600

    Loops

---
+++ First Unity Project/Assets/ForLoop.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ForLoop : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+	{
+		int max = 5;
+
+		for (int i = 0; i < max; i++)
+		{
+			Debug.Log (i + 1);
+		}
+	}
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ First Unity Project/Assets/ForeachLoop.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ForeachLoop : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+	{
+		string[] countdown = new string[5];
+
+		countdown [0] = "FIVE";
+		countdown [1] = "FOUR";
+		countdown [2] = "THREE";
+		countdown [3] = "TWO";
+		countdown [4] = "ONE";
+
+		foreach (string number in countdown)
+		{
+			Debug.Log (number);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
