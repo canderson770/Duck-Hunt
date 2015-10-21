e043d676e4f0aa7d993313b90faab84fcdb67948
unknown <10600006@CS514-13.ad.uvu.edu>
2015-10-21 08:59:49 -0600

    Arrays

---
+++ First Unity Project/Assets/Arrays.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Arrays : MonoBehaviour
+{
+	 int[] myArray = {10, 12, 23, 34};
+
+	// Use this for initialization
+	void Start ()
+	{
+		for (int i =0; i < myArray.Length; i++)
+		{
+			Debug.Log (myArray[i]);
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+}
