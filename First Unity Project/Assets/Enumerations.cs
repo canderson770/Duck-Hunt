4e95ab0000478d97991e251bf2dca184fd5fe3bc
unknown <10600006@CS514-13.ad.uvu.edu>
2015-10-26 10:13:59 -0600

    Enumerations

---
+++ First Unity Project/Assets/Enumerations.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enumerations : MonoBehaviour
+{
+	enum Direction {North, South, East, West};
+
+	Direction ReverseDirection(Direction dir)
+	{
+		if (dir == Direction.North)
+			dir = Direction.South;
+		else if(dir == Direction.South)
+			dir = Direction.North;
+
+		return dir;
+	}
+
+
+	void Start ()
+	{
+		Direction newDirection;
+		newDirection = Direction.North;
+
+		Debug.Log (ReverseDirection(newDirection));
+	}
+	
+	void Update ()
+	{
+	
+	}
+}
