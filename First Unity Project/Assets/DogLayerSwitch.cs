cb0891bc36069e3dca4851755227520b3014fac7
unknown <10600006@CS514-13.ad.uvu.edu>
2015-11-18 10:48:47 -0700

    Duck Hunt v0.3

---
+++ First Unity Project/Assets/DogLayerSwitch.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DogLayerSwitch : MonoBehaviour
+{
+	//void Start () {}
+
+	//void Update () {}
+
+	void SwitchLayer()
+	{
+		SpriteRenderer.
+	}
+}
